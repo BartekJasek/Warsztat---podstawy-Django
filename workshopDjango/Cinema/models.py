afe7a623c23eac596b1602a353f901216fadddff
BartekJasek <bartek.jasek5@gmail.com>
2023-02-07 22:25:04 +0100

    createdmodel

--- workshopDjango/Cinema/models.py
+++ workshopDjango/Cinema/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Hall(models.Model):
+     hall_name = models.CharField(max_length=255)
+     hall_size = models.IntegerField
+     projector = models.BooleanField
